5495fda7b29a223672dee1a31d4f094daad8cd81
ManvendraPSdev <welcome2github@gmail.com>
2026-04-23 17:14:06 +0530

    Add login functionality to authentication controller. Implement loginController to handle user login, including email verification checks and JWT token generation. Update auth routes to include login endpoint.

--- be/src/controllers/auth.controllers.js
+++ be/src/controllers/auth.controllers.js
@@ -2,6 +2,11 @@ import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken" ; 
 import { sendEmail } from "../services/mail.service.js";
 
+/**
+ * @route POST /api/auth/register
+ * @desc register user
+ * @acess Public
+ */
 const registerController = async (req , res)=>{
     try {
         const {userName , email , password} = req.body ; 
@@ -95,6 +100,14 @@ const registerController = async (req , res)=>{
     }
 }
 
+
+/**
+ * @route GET /api/auth/verify-email
+ * @desc Verify user email
+ * @acess Public
+ * @query {token}
+ */
+
 const verifyEmailController = async (req , res)=>{
     const {token} = req.query ; 
 
@@ -119,4 +132,47 @@ const verifyEmailController = async (req , res)=>{
     `)
 }
 
-export {registerController , verifyEmailController}
+
+/**
+ * @route POST /api/auth/login
+ * @desc login user
+ * @acess Public
+ */
+const loginController = async(req , res)=>{
+    const {userName , email , password} = req.body ; 
+
+    const user = await userModel.findOne({email}) ; 
+
+    if(!user){
+        return res.status(404).json({
+            message : "Invalid email or password" , 
+            sucess : false , 
+            err : "user not found"
+        })
+    }
+
+    if(!user.verified){
+        return res.status(400).json({
+            message : "please verify your email before logging in" , 
+            sucess : false ,
+            err : "user is not verified"
+        })
+    }
+
+    const token = jwt.sign({id : user._id , userName : user.userName , email : user.email} , process.env.JWT_SECRET , {expiresIn : '7d'}) ; 
+
+    await res.cookie("token" , token) ; 
+
+    return res.status(200).json({
+        message : "user logged in sucessfully" , 
+        sucess : true ,
+        user : {
+            id : user._id , 
+            name : user.userName , 
+            email : user.email
+        }
+    })
+
+}
+
+export {registerController , verifyEmailController , loginController}
--- be/src/routes/auth.routes.js
+++ be/src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import express from "express" ; 
-import { registerController, verifyEmailController } from "../controllers/auth.controllers.js";
+import { loginController, registerController, verifyEmailController } from "../controllers/auth.controllers.js";
 import validate from "../middlewares/validate.middleware.js";
-import { register } from "../validators/auth.validator.js";
+import { login, register } from "../validators/auth.validator.js";
 
 export const authRouter = express.Router() ; 
 
 authRouter.post("/register" , register , validate , registerController) ; 
 
 authRouter.get("/verify-email" , verifyEmailController)
+
+authRouter.post("/login" , login , validate , loginController)
