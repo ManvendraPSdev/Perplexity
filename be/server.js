5ec910cc935c57c13ee84fbed2a4cc7ea6276fe8
ManvendraPSdev <welcome2github@gmail.com>
2026-04-25 15:41:27 +0530

    Add Socket.io integration: Update package.json and package-lock.json to include socket.io dependency, create server.socket.js for socket initialization, and modify server.js to set up HTTP server with Socket.io support.

--- be/server.js
+++ be/server.js
@@ -2,11 +2,17 @@ import dotenv from "dotenv" ;
 dotenv.config() ; 
 
 import app from "./src/app.js";
+import http from "http" ; // for socket.io
 import connectedToDB from "./src/config/db.js";
+import { initSocket } from "./src/sockets/server.socket.js";
 
 const PORT = process.env.PORT ; 
+
+const httpServer = http.createServer(app) ; 
+initSocket(httpServer) ; 
+
 connectedToDB() ; 
 
-app.listen(PORT , ()=>{
+httpServer.listen(PORT , ()=>{
     console.log(`server is running on PORT ${PORT}`) ; 
 })
---
+++ be/src/sockets/server.socket.js
@@ -0,0 +1,23 @@
+import {Server} from "socket.io" ; 
+
+let io  ; //  the socket.io server is been represented by this io 
+
+export function initSocket(httpServer){
+    io = new Server(httpServer , {
+        cors : {
+            origin : "http://localhost:5173" ,
+            credentials : true
+        }
+    })
+    console.log("Socket.io server is running")
+    io.on("connection" , (socket)=>{
+        console.log("A user connected :"+ socket.id)
+    })
+}
+
+export function getIO(){
+    if(!io){
+        throw new Error("Socket.io is not initialized")
+    }
+    return io ; 
+}
